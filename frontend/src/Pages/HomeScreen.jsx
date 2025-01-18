6d34c2740e923bf215334e8e5fe54d0864baba20
Ayan Saiyed <145615216+AyanASaiyed@users.noreply.github.com>
2025-01-17 22:42:29 -0500

    login state

--- frontend/src/Pages/HomeScreen.jsx
+++ frontend/src/Pages/HomeScreen.jsx
@@ -1,11 +1,35 @@
-import React from "react";
+import { useState } from "react";
+import useLogin from "../Hooks/useLogin";
 
 const HomeScreen = () => {
+  const [username, setUsername] = useState();
+
+  const login = useLogin();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      login(username);
+      console.log("login success");
+    } catch (error) {
+      console.log("Error in HomeScreen Login: ", error.message);
+    }
+  };
+
   return (
-    <div className="flex justify-center flex-center">
-        <h1>You will be debating against a bot and will be up against playersStart by creating a username and clicking 'Start Game' to join a game!</h1>
-      <input className="rounded-xl h-[10vh] w-[30vw]" />
-      <button className="rounded-xl bg-green-700 text-white font-mono text-3xl p-4 hover:bg-green-900">Start Game</button>
+    <div className="flex items-center justify-center h-screen flex-col space-y-4">
+      <form onSubmit={handleLogin}>
+        <input
+          className="rounded-xl h-[10vh] w-[30vw] text-3xl font-mono"
+          value={username}
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+        <button className="rounded-xl bg-green-700 text-white font-mono text-3xl p-4 hover:bg-green-900">
+          Start Game
+        </button>
+      </form>
     </div>
   );
 };
