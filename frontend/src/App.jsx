8e7794508dd608ac03e7121476ddeeb6f5835341
Ayan Saiyed <145615216+AyanASaiyed@users.noreply.github.com>
2025-01-17 20:05:27 -0500

    main screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 function App() {
   return (
-    <div className="flex justify-center items-center flex-col">
-      <div className="text-black font-extrabold">Hello</div>
+    <div className="flex justify-center items-center flex-col p-10">
+      <div className="text-white font-mono text-5xl mt-10">Welcome to Deeb<span className="text-blue-500">Ai</span>t!</div>
+      <div className="h-[80vh] w-[80vw] rounded-xl mt-5 bg-slate-400 bg-opacity-15 border-4 border-white"></div>
     </div>
   );
 }
